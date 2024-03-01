2e83833b89131c208544d658efb518621f5bd61a
furkan-developer <furknn2112@gmail.com>
2024-03-01 16:41:38 +0300

    Modify| Masstransit package has been removed from Order.API project

---
+++ Order.API/Program.cs
@@ -0,0 +1,67 @@
+using Microsoft.EntityFrameworkCore;
+using Order.API.Models.Contexts;
+using Order.API.Models.Entities;
+using Order.API.ViewModels;
+using Shared;
+using Shared.Events;
+using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<OrderDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MSSQLServer")));
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapPost("/create-order", async (CreateOrderVM model, OrderDbContext orderDbContext) =>
+{
+    Order.API.Models.Entities.Order order = new()
+    {
+        BuyerId = model.BuyerId,
+        CreatedDate = DateTime.UtcNow,
+        TotalPrice = model.OrderItems.Sum(oi => oi.Count * oi.Price),
+        OrderItems = model.OrderItems.Select(oi => new Order.API.Models.Entities.OrderItem
+        {
+            Price = oi.Price,
+            Count = oi.Count,
+            ProductId = oi.ProductId,
+        }).ToList(),
+    };
+
+    await orderDbContext.Orders.AddAsync(order);
+    await orderDbContext.SaveChangesAsync();
+
+    var idempotentToken = Guid.NewGuid();
+    OrderCreatedEvent orderCreatedEvent = new()
+    {
+        BuyerId = order.BuyerId,
+        OrderId = order.Id,
+        TotalPrice = model.OrderItems.Sum(oi => oi.Count * oi.Price),
+        OrderItems = model.OrderItems.Select(oi => new Shared.Datas.OrderItem
+        {
+            Price = oi.Price,
+            Count = oi.Count,
+            ProductId = oi.ProductId
+        }).ToList(),
+        IdempotentToken = idempotentToken
+    };
+
+    OrderOutbox orderOutbox = new()
+    {
+        OccuredOn = DateTime.UtcNow,
+        ProcessedDate = null,
+        Payload = JsonSerializer.Serialize(orderCreatedEvent),
+        Type = nameof(OrderCreatedEvent),
+        IdempotentToken = idempotentToken
+    };
+    await orderDbContext.OrderOutboxes.AddAsync(orderOutbox);
+    await orderDbContext.SaveChangesAsync();
+    
+});
+
+app.Run();
